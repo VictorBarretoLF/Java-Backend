6e97c0fff275c897bffb016a3b2ef89c935346dc
Víctor Barreto <victorbarretolins@gmail.com>
2023-01-16 13:37:53 -0300

    creating the get method

--- SpringWebMVC/src/main/java/SpringWebMVC/SpringWebMVC/controller/TutorialController.java
+++ SpringWebMVC/src/main/java/SpringWebMVC/SpringWebMVC/controller/TutorialController.java
@@ -1,11 +1,39 @@
 package SpringWebMVC.SpringWebMVC.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import SpringWebMVC.SpringWebMVC.model.Tutorial;
+import SpringWebMVC.SpringWebMVC.service.TutorialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
+
+    @Autowired
+    TutorialService tutorialService;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<>();
+
+            if (title == null)
+                tutorials.addAll(tutorialService.findAll());
+
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
