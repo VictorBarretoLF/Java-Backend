9debad3a54d52b095039c2f85b5f8b02a80e923e
Víctor Barreto <victorbarretolins@gmail.com>
2023-01-19 16:44:49 -0300

    created a test to check the filter by title

--- SpringWebMVC/src/test/java/SpringWebMVC/SpringWebMVC/controller/TutorialControllerTest.java
+++ SpringWebMVC/src/test/java/SpringWebMVC/SpringWebMVC/controller/TutorialControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
@@ -37,7 +39,7 @@ class TutorialControllerTest {
     @Test
     void shouldReturnTutorial() throws Exception {
         long id = 1L;
-        Tutorial tutorial = new Tutorial(id, "Sprint Test One 1", "Test Description", true);
+        Tutorial tutorial = new Tutorial(id, "Spring Test One 1", "Test Description", true);
 
         when(tutorialService.findById(id)).thenReturn(tutorial);
         mockMvc.perform(get("/api/tutorials/{id}", id)).andExpect(status().isOk())
@@ -61,9 +63,9 @@ class TutorialControllerTest {
     @Test
     void shouldReturnListOfTutorials() throws Exception {
         List<Tutorial> tutorials = new ArrayList<>(
-                Arrays.asList(new Tutorial(1, "Sprint Test One 1", "Test Description 1", true),
-                        new Tutorial(2, "Sprint Test Two 2", "Test Description 2", true),
-                        new Tutorial(3, "Sprint Test Three 3", "Test Description 3", true)));
+                Arrays.asList(new Tutorial(1, "Spring Test One 1", "Test Description 1", true),
+                        new Tutorial(2, "Spring Test Two 2", "Test Description 2", true),
+                        new Tutorial(3, "Spring Test Three 3", "Test Description 3", true)));
 
         when(tutorialService.findAll()).thenReturn(tutorials);
         mockMvc.perform(get("/api/tutorials"))
@@ -72,6 +74,23 @@ class TutorialControllerTest {
                 .andExpect(jsonPath("$.[1].id").value(tutorials.get(1).getId()))
                 .andExpect(jsonPath("$.[1].title").value(tutorials.get(1).getTitle()))
                 .andExpect(jsonPath("$.[1].description").value(tutorials.get(1).getDescription()))
+                .andDo(print());
+    }
+
+    @Test
+    void shouldReturnListOfTutorialsWithFilter() throws Exception {
+        List<Tutorial> tutorials = new ArrayList<>(
+                Arrays.asList(new Tutorial(1, "Spring Test One 1", "Test Description 1", true),
+                        new Tutorial(2, "Spring Test Two 2", "Test Description 2", true)));
+
+        String title = "test";
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+        paramsMap.add("title", title);
+
+        when(tutorialService.findByTitleContainingIgnoreCase(title)).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials").params(paramsMap))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(tutorials.size()))
                 .andDo(print());
     }
 }
